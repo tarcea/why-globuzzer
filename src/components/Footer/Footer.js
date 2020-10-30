ec45c8c67ac2b6caf358813800bfbd6faa1cc816
Gheorghe Tarcea <tarcea@gmail.com>
2020-10-30 14:03:40 +0100

    media queries

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,13 +5,13 @@ import FooterContactMobile from './FooterContactMobile';
 import { useMediaQuery } from 'react-responsive';
 
 const Footer = () => {
-  const isDesktopOrLaptop = useMediaQuery({
-    query: '(min-device-width: 76.5em)'
-  });
-  const isBigScreen = useMediaQuery({
-    query: '(min-device-width: 114em)' });
+  // const isDesktopOrLaptop = useMediaQuery({
+  //   query: '(min-device-width: 76.5em)'
+  // });
+  // const isBigScreen = useMediaQuery({
+  //   query: '(min-device-width: 114em)' });
   const isTabletOrMobile = useMediaQuery({
-    query: '(max-width: 1224px)' });
+    query: '(max-width: 900px)' });
 
   return (
     <div className="footer-container">
