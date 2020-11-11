b4092a8035ecc64243666ca5c982f0badd0cbdd7
Gheorghe Tarcea <tarcea@gmail.com>
2020-11-11 12:46:48 +0100

    used context for implement anchor links, for mobile view

--- src/components/Navbar/Sesame.js
+++ src/components/Navbar/Sesame.js
@@ -2,40 +2,47 @@ import React from 'react';
 import logo from '../../images/globuzzer_logo.png';
 import { AiOutlineQuestionCircle } from 'react-icons/ai';
 import { GoLocation } from 'react-icons/go';
-import {useSpring, animated} from 'react-spring';
+import { AnchorContext } from '../.././contexts/AnchorContext';
+// import {useSpring, animated} from 'react-spring';
 
 const Sesame = ({ openSesame, handleOpen}) => {
-  const springStyle = useSpring({
-                                  opacity: openSesame ? 1 : 0,
-                                })
 
-  const handleClick = () => {
+
+  const handleSesame = () => {
     handleOpen(!openSesame)
   }
 
   return (
-    <animated.div style={springStyle}>
-    <div className="floating-menu">
-      <div className="sesame-container">
-        <div>
-          <img src={logo} alt="logo" className="sesame-logo"/>
-        </div>
-        <div className="sesame-links">
-          <div className="link">
-            <GoLocation />
-            <a href="#www" onClick={handleClick}>Testimonials</a>
+    <AnchorContext.Consumer>{(context) => {
+      const { top, footer, testimonials, } = context;
+
+          const handleClick = (anchor) => {
+            anchor.current.scrollIntoView({behavior: "smooth"});
+        }
+
+          return (
+        <div className="floating-menu">
+          <div className="sesame-container">
+            <div>
+              <img src={logo} alt="logo" className="sesame-logo"/>
+            </div>
+            <div className="sesame-links">
+              <div className="link">
+                <GoLocation />
+                <a href="#www" onClick={handleClick}>Testimonials</a>
+              </div>
+              <div className="link">
+                <AiOutlineQuestionCircle />
+                <a  onClick={() => {handleClick(footer); handleSesame()}} style={{cursor:"pointer"}}>Contact us</a>
+              </div>
+            </div>
           </div>
-          <div className="link">
-            <AiOutlineQuestionCircle />
-            <a href="#footer" onClick={handleClick}>Contact us</a>
+          <div className="ghost-button" style={{color:"#FFFFFF", background:"#F24B6A", margin:"33px auto 0"}}>
+            <p onClick={handleSesame}>Get started</p>
           </div>
         </div>
-      </div>
-      <div className="ghost-button" style={{color:"#FFFFFF", background:"#F24B6A", margin:"33px auto 0"}}>
-        <p onClick={handleClick}>Get started</p>
-      </div>
-    </div>
-    </animated.div>
+        );
+      }}</AnchorContext.Consumer>
   );
 }
 
